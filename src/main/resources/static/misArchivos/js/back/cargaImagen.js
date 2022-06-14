bd1e4fe6093206a0510b2b67df3fa8ad0d51b940
Javier Jimenez Diaz <jjimdia0908@g.educaand.es>
2022-06-14 20:37:51 +0200

    modificando archivos js para mejorar funcionamiento

--- src/main/resources/static/misArchivos/js/back/cargaImagen.js
+++ src/main/resources/static/misArchivos/js/back/cargaImagen.js
@@ -5,7 +5,9 @@ $(function ($) {
          $(".main-panel").remove();
          $("<div></div>").addClass("main-panel").appendTo("#contenedor");
          $(".main-panel").load("/cargaPlantillaImagen/"+ this.id, function(){
-
+            $("#imagen_archivo").change(function(){
+                $("#contenedor_imagen_img").find("img").attr("src", "../../misArchivos/img/"+ $("#imagen_archivo").val().substr(12));
+            })
          })
     })
 
